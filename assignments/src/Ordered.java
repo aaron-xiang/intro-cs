c00cb0f9775e8cb8f5864325492d460df344468e
Aaron Xiang <aaron.xiang@gmail.com>
2019-06-23 10:27:22 -0700

    create assignment01 package

---
+++ assignments/src/Ordered.java
@@ -0,0 +1,9 @@
+public class Ordered {
+    public static void main(String[] args) {
+        StdIn stdIn;
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        System.out.printf(a + ", " + b + ", " + c);
+    }
+}
